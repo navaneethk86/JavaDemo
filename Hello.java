459adde0e4aaec4f870f2bbc5aa7b3a61980ae9e
navaneethk86 <nnm23mc081@nmamit.in>
2024-03-30 15:49:36 +0530

    done

--- Hello.java
+++ Hello.java
@@ -3,11 +3,13 @@ public class Hello
     public static void main(String[] s)
     {
         System.out.println("hello world");
+        Student Stu =new Student();
+        Stu.displayName("ben", 12);
 
     }
 }
 
-class Studen
+class Student
 {
     public void displayName(String name,int age)
     {
